71e72455507da26dec7ea16f359da81e891f43e1
The-Creators-Hand <357179@eriesd.org>
2025-05-09 09:10:43 -0400

    Existence: Crawlers and their Bloaters

--- enemy.py
+++ enemy.py
@@ -216,6 +216,8 @@ class BigEnemy(pygame.sprite.Sprite):
     def __del__(self):
         self.count -= 1 
 
+
+
 class SpeedyBoi(pygame.sprite.Sprite):
 
     #need to count how many exist?
@@ -242,7 +244,7 @@ class SpeedyBoi(pygame.sprite.Sprite):
 
         #initialize its speedy spritesheet
         self.spritesheet = SpriteSheet(r"sprites\Runner.png")
-        self.sprites = self.spritesheet.get_images(0,0,32,32,1)
+        self.sprites = self.spritesheet.get_images(0,0,32,32,4)
         self.image = self.sprites[0]
         
 
@@ -325,6 +327,222 @@ class SpeedyBoi(pygame.sprite.Sprite):
 
     def __del__(self):
         self.count -= 1 
+
+
+
+class Bloater(pygame.sprite.Sprite):
+    count = 0
+    def __init__(self, game):
+        """PHATT BOI"""
+        super().__init__()
+        #initialize game and settings of this CHONKer
+        self.game = game
+        self.settings = self.game.settings
+
+
+
+        #Initialize his BEEFY stats
+        self.hp = 7
+        self.type = "special"
+        self.speed = self.settings.blindBulb_SPEED
+
+
+
+        #set up its spritesheet
+        self.spritesheet = SpriteSheet(r"sprites\machete.png") #SUBJECT TO CHANGE
+        self.sprites = self.spritesheet.get_images(0,0,48,48,1) #SUBJECT TO CHANGE
+        self.image = self.sprites[0]
+        
+
+
+        #define its FAT hitbox based on its spreitesheet
+        self.rect = self.image.get_rect()
+        self.rect.center = (-1000,-1000)
+
+
+
+        #handle its random spawn direction
+        spawn = random.choice(['top', 'bottom', 'left', 'right'])
+        if spawn == 'top':
+            self.x = random.randint(0, self.settings.screen_WIDTH)
+            self.y = 0
+        elif spawn == 'bottom':
+            self.x = random.randint(0, self.settings.screen_WIDTH)
+            self.y = self.settings.screen_HEIGHT
+        elif spawn == 'left':
+            self.x = 0
+            self.y = random.randint(0, self.settings.screen_HEIGHT)
+        elif spawn == 'right':
+            self.x = self.settings.screen_WIDTH
+            self.y = random.randint(0, self.settings.screen_HEIGHT)
+
+
+
+        #handle its frames for its sprites
+        self.frame = 0
+        self.count += 1 
+    
+
+
+    def check_collide(self, player):
+        if player:
+            if self.rect.colliderect(player.rect):
+                if player.shield > 0:
+                    player.shield -=3
+                else:
+                    player.HP -= 3 #maybe we should add a damage variable?
+                self.hp -= 7
+
+
+
+    def knockback (self, bullet):
+        self.x+= bullet.direction[0]*self.settings.KNOCKBACK_AMOUNT
+        self.y+= bullet.direction[1]*self.settings.KNOCKBACK_AMOUNT
+
+
+
+    def update(self, player):
+        """Update the BEEG mf"""
+        if player:  # Ensure player is not None
+            self.target = player.rect.center
+            distance = [
+                self.target[0] - self.x,
+                self.target[1] - self.y
+            ]
+            normalize = math.sqrt(distance[0]**2 + distance[1]**2)
+            
+            if normalize > 0:  # Prevent division by zero
+                self.direction = [distance[0] / normalize, distance[1] / normalize]
+                self.x += self.direction[0] * self.speed
+                self.y += self.direction[1] * self.speed
+            
+            self.rect.topleft = (self.x, self.y)
+
+        if self.game.frame_count % 15 == 0:
+            self.frame = (self.frame+1)% len(self.sprites)
+        self.image = self.sprites[self.frame]
+
+
+
+    def draw(self, game):
+        """Draw them little shits"""
+        game.screen.blit(self.image, self.rect.topleft)
+
+
+
+    def __del__(self):
+        self.count -= 1 
+
+
+
+class Crawler(pygame.sprite.Sprite):
+
+    #need to count how many exist?
+    count = 0
+
+    def __init__(self, game):
+        """Speedy Boi"""
+        super().__init__()
+
+
+
+        #initialize game and settings
+        self.game = game
+        self.settings = self.game.settings
+
+
+
+        #initialize attributes
+        self.hp = 1
+        self.type = "special"
+        self.speed = self.settings.enemy_SPEED
+
+
+
+        #initialize its speedy spritesheet
+        self.spritesheet = SpriteSheet(r"sprites\crawler.png")
+        self.sprites = self.spritesheet.get_images(0,0,32,32,4)
+        self.image = self.sprites[0]
+        
+
+
+        #define hitbox based on speedy sprites 
+        self.rect = self.image.get_rect()
+        self.rect.center = (-1000,-1000)
+
+
+
+        #handle that random spawn direction
+        spawn = random.choice(['top', 'bottom', 'left', 'right'])
+        if spawn == 'top':
+            self.x = random.randint(0, self.settings.screen_WIDTH)
+            self.y = 0
+        elif spawn == 'bottom':
+            self.x = random.randint(0, self.settings.screen_WIDTH)
+            self.y = self.settings.screen_HEIGHT
+        elif spawn == 'left':
+            self.x = 0
+            self.y = random.randint(0, self.settings.screen_HEIGHT)
+        elif spawn == 'right':
+            self.x = self.settings.screen_WIDTH
+            self.y = random.randint(0, self.settings.screen_HEIGHT)
+
+
+
+        #Sprite frame handling
+        self.frame = 0
+        self.count += 1 
+    
+
+
+    def check_collide(self, player):
+        if player:
+            if self.rect.colliderect(player.rect):
+                if player.shield > 0:
+                    player.shield -= 1
+                else:
+                    player.HP -= 1 #maybe we should add a damage variable?
+                self.hp -= 1
+
+
+
+    def knockback (self, bullet):
+        self.x+= bullet.direction[0]*self.settings.KNOCKBACK_AMOUNT
+        self.y+= bullet.direction[1]*self.settings.KNOCKBACK_AMOUNT
+
+
+
+    def update(self, player):
+        """Update the mf"""
+        if player:  # Ensure player is not None
+            self.target = player.rect.center
+            distance = [
+                self.target[0] - self.x,
+                self.target[1] - self.y
+            ]
+            normalize = math.sqrt(distance[0]**2 + distance[1]**2)
+            
+            if normalize > 0:  # Prevent division by zero
+                self.direction = [distance[0] / normalize, distance[1] / normalize]
+                self.x += self.direction[0] * self.speed
+                self.y += self.direction[1] * self.speed
+            
+            self.rect.topleft = (self.x, self.y)
+
+        if self.game.frame_count % 15 == 0:
+            self.frame = (self.frame+1)% len(self.sprites)
+        self.image = self.sprites[self.frame]
+
+
+
+    def draw(self, game):
+        """Draw them little shits"""
+        game.screen.blit(self.image, self.rect.topleft)
+
+
+
+    def __del__(self):
+        self.count -= 1
 
 
 
